6e0c236e811dcaea496c2da7fce6fc669d601fdb
wojiaoguowei <winner-guowei@163.com>
2018-10-25 09:37:59 +0800

    bash practice

---
+++ test.sh
@@ -0,0 +1,13 @@
+echo "Hello World!"
+your_name="tom"
+echo $your_name
+myUrl="http://www.google.com"
+readonly myUrl
+myUrl="http://www.runoob.com"
+string="abcd"
+echo ${#string}
+echo "Shell 传递参数实例！";
+echo "执行的文件名：$0";
+echo "第一个参数为：$1";
+echo "第二个参数为：$2";
+echo "第三个参数为：$3";
